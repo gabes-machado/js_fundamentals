69e2dc37a967d9b464ead9e46bff3df3adcb9db8
machado <machad0.gabriel@outlook.com>
2024-07-30 11:02:59 -0300

    atualização e documentação - functions.js

--- functions.js
+++ functions.js
@@ -1,40 +1,40 @@
-// exemple function
+// Função de exemplo
+// Esta função recebe um texto como parâmetro e imprime esse texto no console.
 
 function printText(text) {
     console.log(text);
 }
 
+// Chamando a função printText com o texto 'hello world!'
 printText('hello world!');
 
-// a function is needed to print the reversed text
+// Uma função é necessária para imprimir o texto invertido
+// Esta função recebe um texto como parâmetro, inverte esse texto e imprime o texto invertido no console.
 
 function printReversedText(text) {
     console.log(text.split('').reverse().join(''));
 }
 
+// Chamando a função printReversedText com o texto 'hello world!'
 printReversedText('hello world!');
 
 /* 
+O que é uma função?
+Em JavaScript, uma função é um bloco de código reutilizável que executa uma tarefa específica. 
+As funções são um dos blocos de construção fundamentais em JavaScript.
 
-what is a function?
-In JavaScript, a function is a block of reusable code that performs a specific task. 
-Functions are one of the fundamental building blocks in JavaScript.
-
-Here is the basic syntax of a function:
-
+Aqui está a sintaxe básica de uma função:
 */
 
 function functionName(parameters) {
-    // code to be executed
+    // código a ser executado
   }
 
 /*
-
-functionName: This is the name of the function.
-parameters: These are inputs to the function. You can add as many parameters as you want, just separate them with a comma.
-The code to be executed by the function is placed inside curly brackets {}.
-For example, a function that adds two numbers could look like this:
-
+functionName: Este é o nome da função.
+parameters: Estes são os inputs para a função. Você pode adicionar quantos parâmetros quiser, basta separá-los com uma vírgula.
+O código a ser executado pela função é colocado dentro de chaves {}.
+Por exemplo, uma função que adiciona dois números poderia ser assim:
 */
 
 function addNumbers(a, b) {
@@ -42,12 +42,29 @@ function addNumbers(a, b) {
   }
 
 /*
-
-You can call or invoke the function using its name followed by 
-parentheses and provide the parameters (if any) inside these parentheses. For example:
-
+Você pode chamar ou invocar a função usando seu nome seguido por 
+parênteses e fornecer os parâmetros (se houver) dentro desses parênteses. Por exemplo:
 */
 
 let sum = addNumbers(5, 10);
-console.log(sum); // Outputs: 15
+console.log(sum); // Saída: 15
 
+// Expressão de função: a principal diferença é o HOISTING 
+// Aqui, a função é atribuída a uma variável. A função só pode ser chamada após a declaração.
+
+let multiplyNumbers = function(a, b) {
+    return a * b;
+  }
+
+let product = multiplyNumbers(5, 10);
+console.log(product); // Saída: 50
+
+// Função de seta
+// É uma maneira mais curta de escrever uma função. Aqui, a palavra-chave 'function' é substituída por '=>'.
+
+let divideNumbers = (a, b) => {
+    return a / b;
+}
+
+let quotient = divideNumbers(10, 5);
+console.log(quotient); // Saída: 2
